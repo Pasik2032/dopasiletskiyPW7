20d2b83e9f4d04fc09bd830a8d920920ba0787ab
Pasiletskiy Daniil <dopasiletskiy@edu.hse.ru>
2022-01-27 23:58:43 +0300

    Yandex Map (5 point)

--- dopasiletskiyPW7/Scenes/MapKitNavigatorScene/NavigatorViewController.swift
+++ dopasiletskiyPW7/Scenes/MapKitNavigatorScene/NavigatorViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreLocation
 import MapKit
+import YandexMapsMobile
 
 class NavigatorViewController: UIViewController {
     
@@ -18,18 +19,23 @@ class NavigatorViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    private let mapView: MKMapView = {
-        let mapView = MKMapView()
-        mapView.layer.masksToBounds = true
-        mapView.layer.cornerRadius = 5
-        mapView.clipsToBounds = false
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        mapView.showsScale = true
-        mapView.showsCompass = true
-        mapView.showsTraffic = true
-        mapView.showsBuildings = true
-        mapView.showsUserLocation = true
-        return mapView
+//    private let mapView: MKMapView = {
+//        let mapView = MKMapView()
+//        mapView.layer.masksToBounds = true
+//        mapView.layer.cornerRadius = 5
+//        mapView.clipsToBounds = false
+//        mapView.translatesAutoresizingMaskIntoConstraints = false
+//        mapView.showsScale = true
+//        mapView.showsCompass = true
+//        mapView.showsTraffic = true
+//        mapView.showsBuildings = true
+//        mapView.showsUserLocation = true
+//        return mapView
+//    }()
+    
+    private let yandexMap: YMKMapView = {
+        let map = YMKMapView()
+        return map
     }()
     
     private let buttonGo: BigButton = BigButton(background: .systemGreen, text: "Go")
@@ -65,8 +71,15 @@ class NavigatorViewController: UIViewController {
     }
     
     private func configureUI() {
-        view.addSubview(mapView)
-        mapView.frame = view.frame
+//        view.addSubview(mapView)
+//        mapView.frame = view.frame
+        view.addSubview(yandexMap)
+        yandexMap.frame = view.frame
+        yandexMap.mapWindow.map.move(
+            with: YMKCameraPosition.init(target: YMKPoint(latitude: 55.751574, longitude: 37.573856), zoom: 15, azimuth: 0, tilt: 0),
+            animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+            cameraCallback: nil)
+        
         let stack = UIStackView(arrangedSubviews: [buttonGo, buttonClear])
         stack.axis = .horizontal
         stack.distribution = .fillEqually
@@ -127,28 +140,74 @@ class NavigatorViewController: UIViewController {
         }
     }
     
+    var drivingSession: YMKDrivingSession?
+    
     func buildPath(){
-        for i in point{
-            print(i.coordinate)
-            mapView.addAnnotation(i)
+
+        let startpoint = YMKPoint(latitude: point[0].coordinate.latitude, longitude: point[0].coordinate.longitude)
+        let endtpoint = YMKPoint(latitude: point[0].coordinate.latitude, longitude: point[1].coordinate.longitude)
+        
+        let requestPoints : [YMKRequestPoint] = [
+            YMKRequestPoint(point: startpoint, type: .waypoint, pointContext: nil),
+            YMKRequestPoint(point: endtpoint, type: .waypoint, pointContext: nil),
+            ]
+        
+        let responseHandler = {(routesResponse: [YMKDrivingRoute]?, error: Error?) -> Void in
+            if let routes = routesResponse {
+                self.onRoutesReceived(routes)
+            } else {
+                self.onRoutesError(error!)
+            }
         }
         
-        let startpoint = MKPlacemark(coordinate: point[0].coordinate)
-        let endpoint = MKPlacemark(coordinate: point[1].coordinate)
-        let request = MKDirections.Request()
-        request.source = MKMapItem(placemark: startpoint)
-        request.destination = MKMapItem(placemark: endpoint)
-        request.transportType = .automobile
+        let drivingRouter = YMKDirections.sharedInstance().createDrivingRouter()
+        drivingSession = drivingRouter.requestRoutes(
+            with: requestPoints,
+            drivingOptions: YMKDrivingDrivingOptions(),
+            vehicleOptions: YMKDrivingVehicleOptions(),
+            routeHandler: responseHandler)
         
-        let direction = MKDirections(request: request)
-        direction.calculate { (respons, error) in
-            guard let respons = respons else {return}
-            for route in respons.routes{
-                self.mapView.addOverlay(route.polyline)
-            }
-            
+//        let startpoint = MKPlacemark(coordinate: point[0].coordinate)
+//        let endpoint = MKPlacemark(coordinate: point[1].coordinate)
+//        let request = MKDirections.Request()
+//        request.source = MKMapItem(placemark: startpoint)
+//        request.destination = MKMapItem(placemark: endpoint)
+//        request.transportType = .automobile
+        
+        
+//        let direction = MKDirections(request: request)
+//        direction.calculate { (respons, error) in
+//            guard let respons = respons else {return}
+//            for route in respons.routes{
+//                self.mapView.addOverlay(route.polyline)
+//            }
+//
+//        }
+    }
+    
+    func onRoutesReceived(_ routes: [YMKDrivingRoute]) {
+        let mapObjects = yandexMap.mapWindow.map.mapObjects
+        for route in routes {
+            mapObjects.addPolyline(with: route.geometry)
         }
     }
+    
+    func onRoutesError(_ error: Error) {
+        let routingError = (error as NSError).userInfo[YRTUnderlyingErrorKey] as! YRTError
+        var errorMessage = "Unknown error"
+        if routingError.isKind(of: YRTNetworkError.self) {
+            errorMessage = "Network error"
+        } else if routingError.isKind(of: YRTRemoteError.self) {
+            errorMessage = "Remote server error"
+        }
+        
+        let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     
     
     @objc func clearButtonWasPressed(){
